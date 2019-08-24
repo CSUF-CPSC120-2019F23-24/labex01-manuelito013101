8d865c4df192c584a7a8f7752bdaa98decd47f01
Manuelito <manuelito@cs104u-1b.acad.fullerton.edu>
2019-08-24 15:02:08 -0700

    changed hours to candy bars

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -5,20 +5,20 @@
 int main()
 {
   double hours, rate, pay;
-
-  // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
+std::cout << "Welcome to the Earnings Calculator!" << std::endl;
+  // Get the nustd::endl;mber of hours worked.
+  std::cout << "How many candy bars did you sell?";
   std::cin >> hours;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
+  std::cout << "How much does the organization earn for each bar sold?";
   std::cin >> rate;
 
   // Calculate the pay.
   pay = hours * rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "Congratulations! You have earned $ for the organization!" << pay << std::endl;
 
   return 0;
 }
